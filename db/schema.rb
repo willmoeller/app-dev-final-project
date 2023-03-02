2f65d9921cb733ebac7134f12d55dc2efb1dc821
Will Moeller <willmoeller@gmail.com>
2023-03-02 01:53:44 +0000

    Add Brand model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_02_012133) do
+ActiveRecord::Schema.define(version: 2023_03_02_015306) do
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.string "website"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
